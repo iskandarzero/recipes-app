7b2d057f8d71d40ddcd3c1a7101e6720b5427c4c
iskandarzero <riderzerotg@gmail.com>
2022-06-21 14:38:34 -0300

    fix req 17

--- src/pages/Foods.js
+++ src/pages/Foods.js
@@ -1,30 +1,12 @@
-import React, { useContext } from 'react';
-import { RecipesContext } from '../context/RecipesContext';
+import React from 'react';
 import Header from '../components/Header';
+import RecipeCard from '../components/RecipeCard';
 
 function Foods() {
-  const { searchResults } = useContext(RecipesContext);
-  let filteredRecipes = [];
-  const maxRecipes = 12;
-  if (searchResults.length > 1) {
-    filteredRecipes = searchResults.slice(0, maxRecipes);
-  }
-
   return (
     <div>
       <Header title="Foods" />
-      <main>
-        {filteredRecipes.map((recipe, index) => (
-          <div data-testid={ `${index}-recipe-card` } key={ index }>
-            <img
-              data-testid={ `${index}-card-img` }
-              src={ recipe.strMealThumb }
-              alt={ recipe.strMeal }
-            />
-            <h3 data-testid={ `${index}-card-name` }>{recipe.strMeal}</h3>
-          </div>
-        ))}
-      </main>
+      <RecipeCard />
     </div>
   );
 }
