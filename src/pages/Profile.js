f1474b0c7f26fc54cf8e7129e14822b67ff4d31b
ellenmelody <mmelody.store@gmail.com>
2022-06-24 17:12:50 -0300

    adiciona pagina profile req 82-87

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Profile() {
+  const [user, setUser] = useState('');
+
+  useEffect(() => {
+    const email = localStorage.getItem('user');
+    setUser(JSON.parse(email));
+  }, []);
+
+  const history = useHistory();
+
+  const clearStorage = () => {
+    localStorage.clear();
+    history.push('/');
+  };
+
   return (
     <div>
       <Header title="Profile" search={ false } />
+      <div data-testid="profile-email">
+        {user.email}
+      </div>
+
+      <form>
+
+        <button
+          data-testid="profile-done-btn"
+          type="button"
+          onClick={ () => history.push('/done-recipes') }
+        >
+          Done Recipes
+        </button>
+
+        <button
+          data-testid="profile-favorite-btn"
+          type="button"
+          onClick={ () => history.push('/favorite-recipes') }
+        >
+          Favorite Recipes
+        </button>
+
+        <button
+          data-testid="profile-logout-btn"
+          type="button"
+          onClick={ clearStorage }
+        >
+          Logout
+        </button>
+
+      </form>
       <Footer />
     </div>
   );
